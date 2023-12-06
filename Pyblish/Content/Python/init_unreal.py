38c3b885bcc628e6422be690814ed6cda31e628a
hannes <hannesdelbeke@gmail.com>
2023-12-06 20:57:06 +0000

    MVP pyblish unreal plugin

---
+++ Pyblish/Content/Python/init_unreal.py
@@ -0,0 +1,35 @@
+# code in init_unreal.py wil run on startup if the plugin is enabled
+import unreal
+
+
+def create_script_editor_button():
+    """Add a tool button to the tool bar"""
+
+    section_name = 'Plugins'
+    se_command = 'import pyblish_lite;pyblish_lite.show()'
+    label = 'Pyblish'
+    tooltip = "Pyblish for Unreal"
+
+    menus = unreal.ToolMenus.get()
+    level_menu_bar = menus.find_menu('LevelEditor.LevelEditorToolBar.PlayToolBar')
+    level_menu_bar.add_section(section_name=section_name, label=section_name)
+
+    entry = unreal.ToolMenuEntry(type=unreal.MultiBlockType.TOOL_BAR_BUTTON)
+    entry.set_label(label)
+    entry.set_tool_tip(tooltip)
+    entry.set_icon('EditorStyle', 'DebugConsole.Icon')
+    entry.set_string_command(
+        type=unreal.ToolMenuStringCommandType.PYTHON,
+        custom_type=unreal.Name(''),  # not sure what this is
+        string=se_command
+    )
+    level_menu_bar.add_menu_entry(section_name, entry)
+    menus.refresh_all_widgets()
+
+## uncomment to add a button to the toolbar to launch your tool
+## don't forget to modify the function
+# create_script_editor_button()
+
+## TODO add code to add to menu
+
+print("Pyblish is enabled")
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# pyblish-unreal
-Pyblish for Unreal Engine 4 &amp; 5
+# Unreal Python plugin template
+A Pyblish plugin for Unreal.  
+
+## install
+- copy the whole `Pyblish` folder to your plugins path. <!-- TODO add example path project / engine -->
+- install the dependencies from `requirements.txt`  <!-- TODO add instructions with example of correct target folder  -->
+
+<!-- TODO add requirements, unreal version, qt, ... -->
